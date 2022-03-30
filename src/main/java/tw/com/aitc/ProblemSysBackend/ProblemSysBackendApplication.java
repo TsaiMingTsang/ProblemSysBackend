0887fa4c6801200107e578ee6244df7f11938a10
TsaiMingTsang <vincent@aitc.com.tw>
2022-03-30 15:58:38 +0800

    first commit

---
+++ src/main/java/tw/com/aitc/ProblemSysBackend/ProblemSysBackendApplication.java
@@ -0,0 +1,13 @@
+package tw.com.aitc.ProblemSysBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemSysBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProblemSysBackendApplication.class, args);
+	}
+
+}
